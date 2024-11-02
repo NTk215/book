b0ccb973ccd295a4363c5a8da1ac308b70c6ec27
NTk215 <kyhp2005@gmail.com>
2024-11-02 17:42:25 +0700

    blt project

---
+++ nguyen-hong-duc/login/login.js
@@ -0,0 +1,17 @@
+document
+  .getElementById("loginForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Ngăn chặn hành động submit mặc định của form
+
+    // Lấy giá trị của các trường nhập
+    const username = event.target.username.value;
+    const password = event.target.password.value;
+
+    // Kiểm tra thông tin đăng nhập
+    if (username === "123@gmail.com" && password === "123456") {
+      // Nếu thông tin đúng, chuyển hướng tới URL mong muốn
+      window.location.href = "../thong_tin/index.html";
+    } else {
+      alert("Tài khoản hoặc mật khẩu không chính xác.");
+    }
+  });
